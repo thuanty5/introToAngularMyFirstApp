6730b41adb4dc9f89ed44e87ac80bc197f0cab62
SheaClose <SheaClose@gmail.com>
2017-09-04 16:27:25 -0500

    I hope this helps

--- todos/myCtrl.js
+++ todos/myCtrl.js
@@ -1,3 +1,26 @@
 angular.module('todos').controller('todoCtrl', function($scope) {
-  $scope.test = 'This is a test';
+  $scope.todos = [
+    'Create a todo List',
+    'Update the todo List',
+    'Remove Todos',
+    'Show Completed Todos'
+  ];
+  $scope.addTodo = function() {
+    $scope.todos.push($scope.newTodo);
+    $scope.newTodo = '';
+  };
+  $scope.removeTodo = function(todo) {
+    $scope.todos = $scope.todos.filter(function(cur) {
+      return cur !== todo;
+    });
+  };
+  $scope.startEdit = function(todo, indx) {
+    $scope.editedTodo = todo;
+    $scope.index = indx;
+    $scope.editing = true;
+  };
+  $scope.modifyTodo = function(todo) {
+    $scope.todos[$scope.index] = todo;
+    $scope.editing = false;
+  };
 });
