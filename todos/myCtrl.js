d3c09cd2826f4590911c2c57f2e639cfebb05979
SheaClose <SheaClose@gmail.com>
2017-09-03 21:40:09 -0500

    done

--- todos/myCtrl.js
+++ todos/myCtrl.js
@@ -1,4 +1,5 @@
 angular.module('myApp').controller('myCtrl', function($scope, $timeout){
+  $scope.bool = false;
   $scope.todos = ['make a todo list', "display my todos", "create new Todos", "remove todos", "strike out"]
   $scope.addNewTodo = function(newTodo){
     $scope.todos.push(newTodo)
