470273f6af7d626e3ea3aa150ae676ead9505cad
Thuan <thuan@Thuans-MacBook-Pro.local>
2017-09-05 11:01:22 -0500

    Shea walkthrough code

--- todos/myCtrl.js
+++ todos/myCtrl.js
@@ -1,1 +1,39 @@
- 
+
+// angular.module("todos").controller("todoCtrl", function($scope){
+
+// 	$scope.todo = [
+// 		"create a todo list",
+// 		"update todo list",
+// 		"remove todos",
+// 		"show completed todos"
+// 	];
+
+// 	$scope.addTodo = function(){
+// 		$scope.todos.push($scope.newTodo);
+// 	}
+
+
+// });
+
+angular.module('todos').controller('todoCtrl', function($scope) {
+	
+	 $scope.todos = [
+	   'Create a todo List',
+	   'Update the todo List',
+	   'Remove Todos',
+	   'Show Completed Todos'
+	 ];
+
+	 $scope.addTodo = function() {
+	   $scope.todos.push($scope.newTodo);
+	   $scope.newTodo = '';
+	 };
+
+	 $scope.removeTodo = function(todo){
+	 	$scope.todos = $scope.todos.filter(function(cur){
+	 		return cur !== todo;
+	 	});
+	 }
+
+	 
+});
